e2c3478dc4f192718fa134afdcf1b3a26dacb598
Jon Gilmore <jonathangilmore@hotmail.com>
2024-01-30 13:39:18 +0800

    new config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-2_IHmKRSAfG', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '511pn7ktg4j3s443p3kfr8h8tv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-southeast-2_U5pfydxpV', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '32gd0vqmq2nvp7jjiblnecst5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
