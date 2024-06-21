37e05c972d2ae423a8e3c07a6d2a05de2df9347e
Jon Gilmore <jonathangilmore@hotmail.com>
2024-06-21 08:42:09 +0800

    calculate button

--- js/ride.js
+++ js/ride.js
@@ -57,7 +57,7 @@ WildRydes.map = WildRydes.map || {};
         $.ajax({
             method: 'GET',
             //url: _config.api.invokeUrl + '/calculate',
-            url: _config.api.invokeUrl + '/team/1',
+            url: _config.api.invokeUrl + '/teams/1',
             headers: {
                 Authorization: authToken
             },
