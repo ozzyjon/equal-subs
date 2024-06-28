24f2ce777b1075642ba7835040e08a4de632736f
Jon Gilmore <jonathangilmore@hotmail.com>
2024-06-28 14:09:34 +0800

    altered cognito user pool settings

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-2_AtMZyDqp3', // e.g. ap-southeast-2_U5pfydxpV
-        userPoolClientId: 'UserPoolClient-kNC8RBlETUrG', // e.g. 32gd0vqmq2nvp7jjiblnecst5
+        userPoolId: 'ap-southeast-2_Oo3wUuBVQ', // e.g. ap-southeast-2_U5pfydxpV
+        userPoolClientId: '622ri3t2danc8j0qc4e0mne54l', // e.g. 32gd0vqmq2nvp7jjiblnecst5
         region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://1pkz4qb5e1.execute-api.ap-southeast-2.amazonaws.com/Prod' // 'https://hqik00klc1.execute-api.ap-southeast-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://1zkttnbil9.execute-api.ap-southeast-2.amazonaws.com/prod' // 'https://hqik00klc1.execute-api.ap-southeast-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
